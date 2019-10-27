c22aa86e33e3cdf987e6a1a4a15c729b0d3675dd
Jesse Luehrs <doy@tozt.net>
2019-10-27 08:33:48 -0400

    update docs for feature

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@
 //! `Parser` and `Creator` can be used to read and write files manually, and
 //! `Reader` and `Writer` are helpers to provide a nicer API for asynchronous
 //! applications using `tokio`.
+//!
+//! If you are not using `tokio`, the `tokio` dependencies can be removed by
+//! building with `default_features = false` (by default, the `"async"`
+//! feature is enabled which provides `tokio` support).
 
 // XXX this is broken with ale
 // #![warn(clippy::cargo)]
